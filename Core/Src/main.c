f4904dd87a9419ddcd99861fd50c1733c6ed5296
Dmitriy <regilsph@gmail.com>
2023-11-21 10:18:13 +0300

    Now works with 2 SPIs

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -41,6 +41,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 SPI_HandleTypeDef hspi1;
+SPI_HandleTypeDef hspi2;
 DMA_HandleTypeDef hdma_spi1_tx;
 DMA_HandleTypeDef hdma_spi1_rx;
 
@@ -54,6 +55,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_SPI1_Init(void);
+static void MX_SPI2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -93,6 +95,7 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_SPI1_Init();
+  MX_SPI2_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -103,13 +106,17 @@ int main(void)
   {
     /* USER CODE END WHILE */
 	  for (int i = 0; i < BUFFER_SIZE; i++) {
-	        transmitBuffer[i] = i;
-	        receiveBuffer[i] = 0;
-	      }
+	          transmitBuffer[i] = i;
+	          receiveBuffer[i] = i + 10;
+	  }
 
-	  	  HAL_SPI_TransmitReceive_DMA(&hspi1, transmitBuffer, receiveBuffer, BUFFER_SIZE);
+	  HAL_SPI_TransmitReceive_DMA(&hspi1, transmitBuffer, receiveBuffer, BUFFER_SIZE);
 
-	      while (HAL_SPI_GetState(&hspi1) != HAL_SPI_STATE_READY);
+	  while (HAL_SPI_GetState(&hspi1) != HAL_SPI_STATE_READY);
+
+	  HAL_SPI_TransmitReceive_DMA(&hspi2, receiveBuffer, transmitBuffer, BUFFER_SIZE);
+
+	  while (HAL_SPI_GetState(&hspi2) != HAL_SPI_STATE_READY);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -192,6 +199,44 @@ static void MX_SPI1_Init(void)
 }
 
 /**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI2_Init(void)
+{
+
+  /* USER CODE BEGIN SPI2_Init 0 */
+
+  /* USER CODE END SPI2_Init 0 */
+
+  /* USER CODE BEGIN SPI2_Init 1 */
+
+  /* USER CODE END SPI2_Init 1 */
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI2_Init 2 */
+
+  /* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
   * Enable DMA controller clock
   */
 static void MX_DMA_Init(void)
@@ -224,6 +269,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
